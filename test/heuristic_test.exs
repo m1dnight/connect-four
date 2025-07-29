d77549123adc3e2279c8e734596b14bfe71ef8aa
Christophe De Troyer <christophe.detroyer@gmail.com>
2025-07-29 11:02:00 +0200

    heuristic tests

--- test/heuristic_test.exs
+++ test/heuristic_test.exs
@@ -1,212 +1,297 @@
-# defmodule C4.Test.Heuristic do
-#   use ExUnit.Case
-#   doctest C4
+defmodule C4.HeuristicTest do
+  use ExUnit.Case
+  doctest C4.Heuristic
 
-#   alias C4.Board
-#   import C4.Board
-#   alias C4.Heuristic
+  alias C4.{Board, Heuristic}
+  import C4.Board
 
-#   describe "losing_moves/2" do
-#     test "no losing moves" do
-#       board = Board.new()
-#       assert [] = Heuristic.losing_moves(board, :yellow)
-#     end
+  describe "winning_moves/2" do
+    test "identifies horizontal winning move" do
+      board = ~b/
+                | | | | | | | |
+                | | | | | | | |
+                | | | | | | | |
+                | | | | | | | |
+                | | | | | | | |
+                |y|y|y| | | | |
+                /
 
-#     test "one losing move" do
-#       board = ~b/
-#               | | | | | | |
-#               | | | | | | |
-#               | | | | | | |
-#               | | | | | | |
-#               | | | | | | |
-#               |r|r| |r| | |
-#               |y|y| |y| | |
-#               /
+      winning_moves = Heuristic.winning_moves(board, :yellow)
+      assert {4, 1} in winning_moves
+    end
 
-#       assert [{3, 1}] = Heuristic.losing_moves(board, :yellow)
-#     end
-#   end
+    test "identifies vertical winning move" do
+      board = ~b/
+                | | | | | | | |
+                | | | | | | | |
+                | | | | | | | |
+                |y| | | | | | |
+                |y| | | | | | |
+                |y| | | | | | |
+                /
 
-#   describe "winning_moves/2" do
-#     test "no winning moves" do
-#       board = Board.new()
-#       assert [] = Heuristic.winning_moves(board, :yellow)
-#     end
+      winning_moves = Heuristic.winning_moves(board, :yellow)
+      assert {1, 4} in winning_moves
+    end
 
-#     test "one winning move" do
-#       board = ~b/
-#               | | | | | | |
-#               | | | | | | |
-#               | | | | | | |
-#               | | | | | | |
-#               | | | | | | |
-#               | | | | | | |
-#               |y|y| |y| | |
-#               /
-#       assert [{3, 1}] = Heuristic.winning_moves(board, :yellow)
-#     end
+    test "identifies diagonal winning move (ascending)" do
+      board = ~b/
+                | | | | | | | |
+                | | | | | | | |
+                |y| | | | | | |
+                |r|y| | | | | |
+                |r|r|y| | | | |
+                |r|r|r| | | | |
+                /
 
-#     test "multiple winning moves" do
-#       board = ~b/
-#               | | | | | | |
-#               | | | | | | |
-#               | | | | | | |
-#               | | | | | | |
-#               |y| | | | |y|
-#               |y| | | | |y|
-#               |y|y| |y| |y|
-#               /
-#       assert [{1, 4}, {3, 1}, {6, 4}] = Heuristic.winning_moves(board, :yellow)
-#     end
-#   end
+      winning_moves = Heuristic.winning_moves(board, :yellow)
+      assert {4, 1} in winning_moves
+    end
 
-#   describe "score_board/2" do
-#     test "no score" do
-#       board = Board.new()
-#       assert 0 = Heuristic.score_board(board, :yellow)
-#     end
+    test "identifies diagonal winning move (descending)" do
+      board = ~b/
+                | | | | | | | |
+                | | | | | | | |
+                | | | |y| | | |
+                | | |y|r| | | |
+                | |y|r|r| | | |
+                | |r|r|r| | | |
+                /
 
-#     test "opponent wins" do
-#       board = ~b/
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               |r|r|r|r| | | |
-#               /
-#       # losing pennalty, and all 5 possible moves are losing moves
-#       assert -1100 = Heuristic.score_board(board, :yellow)
-#     end
+      winning_moves = Heuristic.winning_moves(board, :yellow)
+      assert {1, 1} in winning_moves
+    end
 
-#     test "player wins" do
-#       board = ~b/
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               |y|y|y|y| | | |
-#               /
-#       # winnig score, and placing an extra y on the board makes "two" wins.
-#       assert 1000 = Heuristic.score_board(board, :yellow)
-#     end
+    test "returns empty list when no winning moves available" do
+      board = Board.new()
+      winning_moves = Heuristic.winning_moves(board, :yellow)
+      assert winning_moves == []
+    end
 
-#     test "one move away from winning" do
-#       board = ~b/
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               |y|y| |y| | | |
-#               /
-#       # winnig score, and placing an extra y on the board makes "two" wins.
-#       assert 500 = Heuristic.score_board(board, :yellow)
-#     end
+    test "identifies multiple winning moves" do
+      board = ~b/
+                | | | | | | | |
+                | | | | | | | |
+                | | | | | | | |
+                |y| | | | | | |
+                |y| | | | | | |
+                |y|y|y| | | | |
+                /
 
-#     test "two ways to win" do
-#       board = ~b/
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               |y| | | | | | |
-#               |y| | | | | | |
-#               |y|y| |y| | | |
-#               /
-#       # winnig score, and placing an extra y on the board makes "two" wins.
-#       assert 1000 = Heuristic.score_board(board, :yellow)
-#     end
+      winning_moves = Heuristic.winning_moves(board, :yellow)
+      assert {1, 4} in winning_moves
+      assert {4, 1} in winning_moves
+    end
+  end
 
-#     test "five ways to win" do
-#       board = ~b/
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               |y| |y|y|y| | |
-#               |y|r|y|r|y|r| |
-#               |y|r|y|r|y|r| |
-#               /
-#       # winnig score, and placing an extra y on the board makes "two" wins.
-#       assert 2500 = Heuristic.score_board(board, :yellow)
-#     end
+  describe "losing_moves/2" do
+    test "identifies moves that allow opponent to win" do
+      board = ~b/
+                | | | | | | | |
+                | | | | | | | |
+                | | | | | | | |
+                | | | | | | | |
+                |r|r|r| | | | |
+                |y|y|y| | | | |
+                /
 
-#     @tag :this
-#     test "figure out best move" do
-#       board = ~b/
-#               | | |y|y| | | |
-#               | | |r|r| | | |
-#               | | |y|y| | | |
-#               | | |r|r| | | |
-#               | |r|r|y| | | |
-#               | |y|y|y| | | |
-#               /
-#       # winnig score, and placing an extra y on the board makes "two" wins.
-#       assert 1000 = Heuristic.score_board(board, :yellow)
-#     end
-#   end
+      losing_moves = Heuristic.losing_moves(board, :yellow)
+      # Playing anywhere except position {4, 1} would allow red to win
+      assert {4, 1} in losing_moves
+    end
 
-#   describe "score_move/3" do
-#     test "no score" do
-#       board = Board.new()
-#       assert 0 = Heuristic.score_move(board, {1, 1}, :yellow)
-#     end
+    test "returns empty list when no moves allow opponent to win" do
+      board = Board.new()
+      losing_moves = Heuristic.losing_moves(board, :yellow)
+      assert losing_moves == []
+    end
 
-#     test "making two consecutive tokens doesnt improve odds" do
-#       board = ~b/
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               |y| | | | | | |
-#               /
-#       assert 0 = Heuristic.score_move(board, {2, 1}, :yellow)
-#     end
+    test "all moves are losing moves if the opponent can win in 1 game" do
+      board = ~b/
+                  | | | | | | | |
+                  | | | | | | | |
+                  | | | | | | | |
+                  |r| | | | | | |
+                  |r| | | | | | |
+                  |r|y| | | | | |
+                  /
 
-#     test "making three improves score by 100" do
-#       board = ~b/
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               |y|y| | | | | |
-#               /
-#       assert 0 = Heuristic.score_move(board, {1, 2}, :yellow)
-#       assert 0 = Heuristic.score_move(board, {2, 2}, :yellow)
-#       assert 0 = Heuristic.score_move(board, {5, 1}, :yellow)
-#       assert 0 = Heuristic.score_move(board, {6, 1}, :yellow)
-#       # these moves lead to a win the next turn
-#       assert 500 = Heuristic.score_move(board, {3, 1}, :yellow)
-#       assert 500 = Heuristic.score_move(board, {4, 1}, :yellow)
-#     end
+      losing_moves = Heuristic.losing_moves(board, :yellow)
+      # Playing in column 1 would stack on top and allow red to win
+      refute {1, 4} in losing_moves
+    end
+  end
 
-#     test "making four is winning score" do
-#       board = ~b/
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               | | | | | | | |
-#               |y|y|y| | | | |
-#               /
-#       assert 1000 = Heuristic.score_move(board, {4, 1}, :yellow)
-#     end
+  describe "rate_series/3" do
+    test "scores 1 token with 3 empty as 10" do
+      board = Board.new()
+      board = Board.put(board, {1, 1}, :yellow)
 
-#     @tag :thiss
-#     test "debug test" do
-#       board = ~b/
-#               | | |r|r| | | |
-#               | | |r|r| | | |
-#               | | |y|y| | | |
-#               | | |r|r| | | |
-#               | |r|r|y| | | |
-#               | |y|y|y| | | |
-#               /
+      positions = [{1, 1}, {2, 1}, {3, 1}, {4, 1}]
+      score = Heuristic.rate_series(board, positions, :yellow)
+      assert score == 10.0
+    end
 
-#       Board.playable_positions(board)
-#       |> Enum.map(&{&1, Heuristic.score_move(board, &1, :yellow)})
-#     end
-#   end
-# end
+    test "scores 2 tokens with 2 empty as 100" do
+      board = Board.new()
+      board = Board.put(board, {1, 1}, :yellow)
+      board = Board.put(board, {2, 1}, :yellow)
+
+      positions = [{1, 1}, {2, 1}, {3, 1}, {4, 1}]
+      score = Heuristic.rate_series(board, positions, :yellow)
+      assert score == 100.0
+    end
+
+    test "scores 3 tokens with 1 empty as 1000" do
+      board = Board.new()
+      board = Board.put(board, {1, 1}, :yellow)
+      board = Board.put(board, {2, 1}, :yellow)
+      board = Board.put(board, {3, 1}, :yellow)
+
+      positions = [{1, 1}, {2, 1}, {3, 1}, {4, 1}]
+      score = Heuristic.rate_series(board, positions, :yellow)
+      assert score == 1000.0
+    end
+
+    test "scores 4 tokens (winning) as 10000" do
+      board = Board.new()
+      board = Board.put(board, {1, 1}, :yellow)
+      board = Board.put(board, {2, 1}, :yellow)
+      board = Board.put(board, {3, 1}, :yellow)
+      board = Board.put(board, {4, 1}, :yellow)
+
+      positions = [{1, 1}, {2, 1}, {3, 1}, {4, 1}]
+      score = Heuristic.rate_series(board, positions, :yellow)
+      assert score == 10000.0
+    end
+
+    test "scores blocked series as 0" do
+      board = Board.new()
+      board = Board.put(board, {1, 1}, :yellow)
+      board = Board.put(board, {2, 1}, :red)
+      board = Board.put(board, {3, 1}, :yellow)
+
+      positions = [{1, 1}, {2, 1}, {3, 1}, {4, 1}]
+      score = Heuristic.rate_series(board, positions, :yellow)
+      assert score == 0.0
+    end
+
+    test "scores empty series as 0" do
+      board = Board.new()
+
+      positions = [{1, 1}, {2, 1}, {3, 1}, {4, 1}]
+      score = Heuristic.rate_series(board, positions, :yellow)
+      assert score == 0.0
+    end
+  end
+
+  describe "score_board/2" do
+    test "scores empty board as 0" do
+      board = Board.new()
+      score = Heuristic.score_board(board, :yellow)
+      assert score == 0
+    end
+
+    test "gives very negative score when opponent can win immediately" do
+      board = ~b/
+                  | | | | | | | |
+                  | | | | | | | |
+                  | | | | | | | |
+                  | | | | | | | |
+                  | | | | | | | |
+                  |r|r|r| | | | |
+                  /
+
+      score = Heuristic.score_board(board, :yellow)
+      assert score == -11170.0
+    end
+
+    test "scores player advantage positively" do
+      board = ~b/
+                  | | | | | | | |
+                  | | | | | | | |
+                  | | | | | | | |
+                  | | | | | | | |
+                  | | | | | | | |
+                  |y|y| | | | | |
+                  /
+
+      score = Heuristic.score_board(board, :yellow)
+      assert score > 0
+    end
+
+      test "scores opponent advantage negatively" do
+        board = ~b/
+                  | | | | | | | |
+                  | | | | | | | |
+                  | | | | | | | |
+                  | | | | | | | |
+                  | | | | | | | |
+                  |r|r|r| |y| | |
+                  /
+
+        score = Heuristic.score_board(board, :yellow)
+        # Red has 3 in a row (threat), yellow has 1 token - should be negative
+        assert score < 0
+      end
+
+      test "considers all winning combinations" do
+        board = ~b/
+                  | | | | | | | |
+                  | | | | | | | |
+                  | | | | | | | |
+                  |y| | | | | | |
+                  |y| | | | | | |
+                  |y|r| | | | | |
+                  /
+
+        score = Heuristic.score_board(board, :yellow)
+        # Yellow has strong vertical position
+        assert score > 0
+      end
+  end
+
+  describe "score_move/3" do
+    test "scores move by evaluating resulting board position" do
+      board = ~b/
+                | | | | | | | |
+                | | | | | | | |
+                | | | | | | | |
+                | | | | | | | |
+                | | | | | | | |
+                |y|y| | | | | |
+                /
+
+      # Making a winning move should score very highly
+      score = Heuristic.score_move(board, {3, 1}, :yellow)
+      assert score > 1000
+    end
+
+    test "scores blocking opponent win highly" do
+      board = ~b/
+                | | | | | | | |
+                | | | | | | | |
+                | | | | | | | |
+                | | | | | | | |
+                | | | | | | | |
+                |r|r|r| | | | |
+                /
+
+      # Blocking red's win should be scored better than not blocking
+      blocking_score = Heuristic.score_move(board, {4, 1}, :yellow)
+      non_blocking_score = Heuristic.score_move(board, {5, 1}, :yellow)
+
+      assert blocking_score > non_blocking_score
+    end
+
+    test "prefers center moves in empty board" do
+      board = Board.new()
+
+      center_score = Heuristic.score_move(board, {4, 1}, :yellow)
+      edge_score = Heuristic.score_move(board, {1, 1}, :yellow)
+
+      # Center moves are generally better strategically
+      assert center_score >= edge_score
+    end
+  end
+end
