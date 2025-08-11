cd2acbe6bd59405dbc582d73ab7f756a3cd5deb1
Christophe De Troyer <christophe.detroyer@gmail.com>
2025-08-11 14:14:19 +0200

    garnish works

--- lib/c4/application.ex
+++ lib/c4/application.ex
@@ -5,9 +5,30 @@ defmodule C4.Application do
 
   @impl true
   def start(_type, _args) do
+    opts = [
+      system_dir:
+        :code.priv_dir(:connect_four)
+        |> Path.join(".keys")
+        |> Path.expand()
+        |> to_charlist,
+      ssh_cli: {Garnish, app: C4.Tui},
+      # ssh_cli: {Garnish, app: Counter},
+      no_auth_needed: true
+    ]
+
+    {:ok, ref} = :ssh.daemon({127, 0, 0, 1}, 2222, opts)
+
     children = []
 
     opts = [strategy: :one_for_one, name: C4.Supervisor]
-    Supervisor.start_link(children, opts)
+
+    with {:ok, pid} <- Supervisor.start_link(children, opts) do
+      {:ok, pid, ref}
+    end
+  end
+
+  @impl true
+  def stop(ref) do
+    :ssh.stop_daemon(ref)
   end
 end
